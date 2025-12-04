7b02287dabd27a8cf27e792ac5c5caa6fd36f7bc
EscoffierZhou <3416270780@qq.com>
2025-12-04 09:59:48 +0800

    (FP16)Add Advanced OStracker

--- CV_TRACKING/trackers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Tracker module initialization
-"""
-from .csrt_tracker import CSRTTracker
-from .kalman_filter import KalmanTracker
-
-__all__ = ['CSRTTracker', 'KalmanTracker']
--- CV_TRACKING/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Utils initialization
-"""
-from .download_test_video import download_test_videos
-
-__all__ = ['download_test_videos']
